ce2dad2e53c2a79a5cac3a100e9038bf9f916fa2
Edith2323 <e.githinji1@alustudent.com>
2024-09-14 11:13:59 -0700

    pep validation

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Module for minimum number of operations to reach n 'H' characters.
+Module to calculate the minimum number of operations to result in exactly
+n 'H' characters in a text file.
 """
 
 
@@ -9,15 +10,16 @@ def minOperations(n):
     Calculates the minimum number of operations needed to result in exactly
     n 'H' characters in the file.
 
-    Operations:
-    - Copy All: copy all the characters in the file
-    - Paste: paste the copied characters
+    Operations allowed:
+    - Copy All: copy all the characters in the file.
+    - Paste: paste the copied characters.
 
     Args:
-    n (int): Target number of 'H' characters.
+        n (int): The target number of 'H' characters.
 
     Returns:
-    int: Minimum number of operations, or 0 if it's impossible to achieve n characters.
+        int: Minimum number of operations to reach exactly n characters, or
+        0 if it's impossible to achieve n characters.
     """
     if n <= 1:
         return 0
@@ -25,11 +27,10 @@ def minOperations(n):
     operations = 0
     factor = 2
 
-    # We keep dividing n by its factors until n becomes 1
+    # Divide n by its factors until n becomes 1
     while n > 1:
-        # If n is divisible by the factor, perform Paste operation
         while n % factor == 0:
-            operations += factor  # Adding the factor as the number of operations
+            operations += factor
             n //= factor
         factor += 1
 
