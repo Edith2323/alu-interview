5f89c13e17e1d7fd93de379b761f7ca6fa271433
Edith2323 <e.githinji1@alustudent.com>
2024-09-14 11:10:12 -0700

    0-main.py

--- minimum_operations/0-main.py
+++ minimum_operations/0-main.py
@@ -1,13 +1,35 @@
 #!/usr/bin/python3
 """
-Main file for testing
+Main file for testing the minOperations function.
 """
 
 minOperations = __import__('0-minoperations').minOperations
 
-n = 4
-print("Min number of operations to reach {} char: {}".format(n, minOperations(n)))
+# Test case 21
+n = 21
+print("Min number of operations to reach {} chars: {}".format(n, minOperations(n)))
 
-n = 12
-print("Min number of operations to reach {} char: {}".format(n, minOperations(n)))
+# Test case 19170307
+n = 19170307
+print("Min number of operations to reach {} chars: {}".format(n, minOperations(n)))
+
+# Test case 972
+n = 972
+print("Min number of operations to reach {} chars: {}".format(n, minOperations(n)))
+
+# Test case 1 (edge case)
+n = 1
+print("Min number of operations to reach {} chars: {}".format(n, minOperations(n)))
+
+# Test case 0 (impossible case)
+n = 0
+print("Min number of operations to reach {} chars: {}".format(n, minOperations(n)))
+
+# Test case -12 (impossible case)
+n = -12
+print("Min number of operations to reach {} chars: {}".format(n, minOperations(n)))
+
+# Test case 2147483640 (large number)
+n = 2147483640
+print("Min number of operations to reach {} chars: {}".format(n, minOperations(n)))
 
--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,15 +1,35 @@
 #!/usr/bin/python3
+"""
+Module for minimum number of operations to reach n 'H' characters.
+"""
+
 
 def minOperations(n):
+    """
+    Calculates the minimum number of operations needed to result in exactly
+    n 'H' characters in the file.
+
+    Operations:
+    - Copy All: copy all the characters in the file
+    - Paste: paste the copied characters
+
+    Args:
+    n (int): Target number of 'H' characters.
+
+    Returns:
+    int: Minimum number of operations, or 0 if it's impossible to achieve n characters.
+    """
     if n <= 1:
         return 0
 
     operations = 0
     factor = 2
 
+    # We keep dividing n by its factors until n becomes 1
     while n > 1:
+        # If n is divisible by the factor, perform Paste operation
         while n % factor == 0:
-            operations += factor
+            operations += factor  # Adding the factor as the number of operations
             n //= factor
         factor += 1
 
