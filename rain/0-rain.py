c9e78e285f4c99b8b5c1c79f8670f6c91f7cb708
Edith2323 <153744079+Edith2323@users.noreply.github.com>
2024-10-01 10:25:47 +0300

    added documentation

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""
+This module contains the 'rain' function which calculates
+the amount of water retained after rainfall, given a list
+of wall heights.
+"""
+
 def rain(walls):
     if not walls:
         return 0
@@ -11,10 +17,10 @@ def rain(walls):
         if left_max < right_max:
             left += 1
             left_max = max(left_max, walls[left])
-            water_retained += left_max - walls[left]
+            water_retained += max(0, left_max - walls[left])
         else:
             right -= 1
             right_max = max(right_max, walls[right])
-            water_retained += right_max - walls[right]
+            water_retained += max(0, right_max - walls[right])
     
     return water_retained
