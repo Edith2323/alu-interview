72d606f6637bd562f5e570d678297ed0ab0503db
Edith2323 <153744079+Edith2323@users.noreply.github.com>
2024-10-01 10:41:13 +0300

    code updated to follow pep 8 validation style

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -5,14 +5,27 @@ the amount of water retained after rainfall, given a list
 of wall heights.
 """
 
+
 def rain(walls):
+    """
+    This function calculates how many square units of water
+    will be retained after it rains, given a list of walls
+    with non-negative integer heights.
+
+    Args:
+        walls (list): List of non-negative integers representing
+                      wall heights.
+
+    Returns:
+        int: The total amount of rainwater retained.
+    """
     if not walls:
         return 0
-    
+
     left, right = 0, len(walls) - 1
     left_max, right_max = walls[left], walls[right]
     water_retained = 0
-    
+
     while left < right:
         if left_max < right_max:
             left += 1
@@ -22,5 +35,5 @@ def rain(walls):
             right -= 1
             right_max = max(right_max, walls[right])
             water_retained += max(0, right_max - walls[right])
-    
+
     return water_retained
